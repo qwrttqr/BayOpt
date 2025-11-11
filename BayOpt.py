e82f82b7070cf5be54b178f84750163045d2f8c9
Petya <alekseenkopert25@gmail.com>
2025-11-11 22:25:46 +0500

    feat: Added BayOpt.py

--- BayOpt.py
+++ BayOpt.py
@@ -7,20 +7,21 @@ class BayesianOptimizer:
     Class for performing Bayesian optimization.
 
     Args:
-        surrogate_function (Callable): The mean function.
-        acquisition_function (Callable): Kernel function used to compute correlations.
+        surrogate_kernel: Kernel of optimization. Should be instantiable and should implement fit predict methods
         target_function (Callable): Kernel function used to compute correlations.
         max_iter (int): Optional. Max iteration count for optimization.
         stop_epsilon (float): Optional. Difference between y_i and y_i+1 iteration of evaluating target. If
         difference is smaller than this quantity - optimization is stopped.
     """
-    def __init__(self, surrogate_function: Callable, acquisition_function: Callable, target_function: Callable,
-                 max_iter=100, stop_epsilon=0.01):
-        self.mean_function = surrogate_function
-        self.corr_func = acquisition_function
+
+    def __init__(self, target_function: Callable, surrogate_kernel: Callable,
+                 max_iter: int = 100, stop_epsilon: float = 0.01):
+        if not(callable(getattr(surrogate_kernel, 'fit', None))) or not(callable(getattr(surrogate_kernel, 'predict', None))):
+            raise AttributeError('Surrogate kernel does not has desired methods')
+        self.surrogate_kernel = surrogate_kernel
         self.target_function = target_function
         self.max_iter = max_iter
-        self.stop_epsilon = 0.01
+        self.stop_epsilon = stop_epsilon
 
 
     def optimize(self, x: np.ndarray, y: np.ndarray):
@@ -29,10 +30,16 @@ class BayesianOptimizer:
         Args:
             x (numpy.ndarray): Values on which target function already been evaluated.
             y (numpy.ndarray): Values of target function gotten from evaluating target function on x values.
-
         Returns:
-
         """
+
+        predictions, stds = self.surrogate_kernel.fit(x,y)
+        current_val = max(y)
+
+        for i in range(self.max_iter):
+            Xsamples = random(100)
+            Xsamples = Xsamples.reshape(len(Xsamples), 1)
+
 
 def priormean(xin, **kwargs):
     c2 = kwargs['c2']
