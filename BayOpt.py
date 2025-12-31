6f71dd811ac39885c7e082a083620e47dfe0cd81
qwrttqr <alekseenkopetr25@gmail.com>
2025-12-31 21:35:59 +0500

    Expaneded bayession optimization on multidimensional cases

--- BayOpt.py
+++ BayOpt.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 import scipy
 
@@ -19,32 +18,41 @@ class BayesianOptimizer:
     """
 
     def __init__(self, target_function: Callable, surrogate_kernel: Any,
-                 max_iter: int = 100, stop_epsilon: float = 0.01):
+                 max_iter: int = 100, stop_epsilon: float = 0.01, optimization_type: str = 'max'):
         if not (callable(getattr(surrogate_kernel, 'fit', None))) or not (
                 callable(getattr(surrogate_kernel, 'predict', None))):
             raise AttributeError('Surrogate kernel does not has desired methods')
+        if optimization_type not in ['max', 'min']:
+            raise Exception('Unsupported optimization type')
+        self.optimization_type = optimization_type
         self.surrogate_kernel = surrogate_kernel
         self.target_function = target_function
         self.max_iter = max_iter
         self.stop_epsilon = stop_epsilon
 
-    def optimize(self, x: np.ndarray, y: np.ndarray, optimization_borders: np.ndarray, uniform_step: float = 0.05):
+    def optimize(self, x: np.ndarray, y: np.ndarray, optimization_borders: np.ndarray, n_samples: int = 1000):
         """
         Perform Bayesian optimization loop.
         """
         self.surrogate_kernel.fit(x, y)
-        ybest = np.max(y)
+        ybest = np.max(y) if self.optimization_type == 'max' else np.min(y)
 
         for i in range(self.max_iter):
             # Generate candidate samples
-            Xsamples = np.arange(optimization_borders[0], optimization_borders[1], uniform_step).reshape(-1, 1)
+            d = optimization_borders.shape[0]
+
+            Xsamples = np.random.uniform(
+                low = optimization_borders[:, 0],
+                high = optimization_borders[:, 1],
+                size = (n_samples, d)
+            )
             # Predict mean and std for each candidate
             new_preds, new_stds = self.surrogate_kernel.predict(Xsamples, return_stds=True)
             # Compute acquisition: probability of improvement
             probs = scipy.stats.norm.cdf((new_preds - ybest) / (new_stds + 1e-9))
             # Select best candidate
             ix = np.argmax(probs)
-            best_x = Xsamples[ix, 0]
+            best_x = Xsamples[ix]
             # Evaluate real objective
             new_y = self.target_function(best_x)
             # Add to training data
@@ -53,8 +61,7 @@ class BayesianOptimizer:
             # Update GP
             self.surrogate_kernel.fit(x, y)
             # Update current best
-            ybest = np.max(y)
-            print(ybest)
+            ybest = np.max(y) if self.optimization_type == 'max' else np.min(y)
 
         return x, y
 
@@ -63,15 +70,7 @@ def objective(x, noise=0.1):
     noise = np.random.normal(loc=0, scale=noise)
     return -x**2 + 10234 + noise
 
-def priormean(xin, c2 = 0.25):
-    return c2 * xin ** 2
 
-def corr_func(a: np.ndarray, b: np.ndarray, sigmaf2=1.0, ell=1.0):
-    a = a.ravel()
-    b = b.ravel()
-    diff = a[:, None] - b[None, :]
-    K = sigmaf2 * np.exp(- (diff ** 2) / (2 * ell ** 2))
-    return K
 
 
 gpReg = GPRegressor({'c2': 0.25}, {'ell': 1, 'sigmaf2': 2}, priormean, corr_func, 0.005)
@@ -81,4 +80,3 @@ BayOpt = BayesianOptimizer(objective, gpReg)
 x = np.arange(0, 1, 0.01).reshape(-1, 1)
 y = np.array([objective(i, 0) for i in x])
 
-print(BayOpt.optimize(x, y, np.array([-100, 100])))
